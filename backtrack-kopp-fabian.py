f6f47b3f3f7335ba0a0266ca95667961d06524e4
Matthew Fabian <mfabian1@matthews-mbp.dhcp.nd.edu>
2017-09-27 23:15:37 -0400

    added copy of brute

--- backtrack-kopp-fabian.py
+++ backtrack-kopp-fabian.py
@@ -18,63 +18,71 @@ def flip(value):
 		return -1
 
 def try_values(temp_values, clause_num, var_num, prob_num, lit_num, expected):
-  # Call variables as globals
-  global sat_num
-  global unsat_num
-  global ans_prov
-  global correct_num
+  	# Call variables as globals
+  	global sat_num
+  	global unsat_num
+  	global ans_prov
+ 	global correct_num
 
-  print "Temp values: " + str(temp_values)
-  print "clause_num: " + str(clause_num)
-  print "var_num: " + str(var_num)
-  print "prob_num: " + str(prob_num)
-  print "lit_num: " + str(lit_num)
-  print "expected: " + str(expected)
+  	print "Temp values: " + str(temp_values)
+  	print "clause_num: " + str(clause_num)
+  	print "var_num: " + str(var_num)
+  	print "prob_num: " + str(prob_num)
+  	print "lit_num: " + str(lit_num)
+  	print "expected: " + str(expected)
 
-  # Initialize stack data structure to contain lists of (variable, value, flag)
-  # flag is 0 if one value has been tried, 1 when both values have been tried
-  stack = [(1, 0, 0)]
-  clauses = []
-  clause_truth_values = []
+  	# Initialize stack data structure to contain lists of (variable, value, flag)
+  	# flag is 0 if one value has been tried, 1 when both values have been tried
+  	stack = [(1, 0, 0)]
 
-  for i, line in enumerate(temp_values):
-  	#print line
-  	clauses.append(line)
-  	for j, variable in enumerate(line):
-  		assigned_vals = [x[0] for x in stack] 
-  		if abs(int(variable)) in assigned_vals:
-  			if int(variable) < 0:
-  				clauses[i][j] = flip(stack[abs(int(variable))-1][1])
-  			else:
-				clauses[i][j] = stack[abs(int(variable))-1][1]
-  
-  # Identify unknown values as -1
-  for i, line in enumerate(clauses):
-  	for j, variable in enumerate(line):
-  		if variable != 0 and variable != 1:
-  			clauses[i][j] = -1
+  	while len(stack) <= var_num:
+		clauses = []
+		clause_truth_values = []
 
-  clause_truth_values = verify(clauses, clause_truth_values)
-  
-  satisfied = 1
-  failed = 0
-  for value in clause_truth_values:
-  	if value == -1:
-  		satisfied = 0
-  	elif value == 0:
-  		failed = 1
-  		satisfied = 0
+		for i, line in enumerate(temp_values):
+			#print line
+			clauses.append(line)
+			for j, variable in enumerate(line):
+				assigned_vals = [x[0] for x in stack] 
+				if abs(int(variable)) in assigned_vals:
+					if int(variable) < 0:
+						clauses[i][j] = flip(stack[abs(int(variable))-1][1])
+					else:
+						clauses[i][j] = stack[abs(int(variable))-1][1]
 
-  if satisfied == 1:
-  	print "SUCCESS"
-  elif failed == 1:
-  	print "GO BACK"
-  else:
-  	print "CONTINUE"
+		# Identify unknown values as -1
+		for i, line in enumerate(clauses):
+			for j, variable in enumerate(line):
+				print variable
+				if variable != 0 and variable != 1:
+					clauses[i][j] = -1
+
+		clause_truth_values = verify(clauses, clause_truth_values)
+
+		satisfied = 1
+		failed = 0
+		for value in clause_truth_values:
+			if value == -1:
+				satisfied = 0
+			elif value == 0:
+				failed = 1
+				satisfied = 0
+
+		if satisfied == 1:
+			#print clauses
+			print clause_truth_values
+			print stack
+			print "SUCCESS"
+			return
+		elif failed == 1:
+			print "GO BACK"
+			return
+		else:
+			stack.append((len(stack)+1,0,0))
 
 
 def verify(clauses, clause_truth_values):
-	
+	print clauses
 	for i, line in enumerate(clauses):
 		clause_truth_values.append(0)
 		for j, variable in enumerate(line):
